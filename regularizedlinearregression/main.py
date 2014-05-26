45097d7da14802b54dd2d090d61a090d4dfdba25
Shantanu <shan_mbic@rediffmail.com>
2014-05-26 16:06:19 +0530

    Regularized linear regression

--- regularizedlinearregression/main.py
+++ regularizedlinearregression/main.py
@@ -31,6 +31,21 @@ def train(X, y, lamda):
 	result = scipy.optimize.fmin_cg( computeCost, fprime = computeGrad, x0 = theta,args = (X, y, lamda), maxiter = 200, disp = True, full_output = True )
 	return result[1], result[0]
 
+
+def polyFeatures(X, p):
+	out = copy(X)
+	for i in range(1, p):
+		out = c_[out, X**(i+1)]
+	return out
+
+def featureNormalize(data):
+	mu = mean( data, axis=0 )
+	data_norm = data - mu
+	sigma = std( data_norm, axis=0, ddof=1 )
+	data_norm = data_norm / sigma
+	return data_norm, mu, sigma
+
+
 def computeCost(theta, X, y, lamda):
 	theta = theta.reshape( shape(X)[1], 1 )
 	m = shape( X )[0]
@@ -55,6 +70,13 @@ def linearRegCostfunction(theta, X, y, lamda):
 	cost=computeCost(theta, X, y, lamda)
 	grad=computeGrad(theta, X, y, lamda)
 	return cost , grad 
+
+def plotFit(min_x, max_x, mu, sigma, theta, p):
+	x = arange( min_x - 15, max_x + 25, 0.05 )
+	X_poly = polyFeatures( x, p )
+	X_poly = (X_poly - mu) / sigma
+	X_poly = c_[ ones((shape(x)[0], 1)), X_poly ]
+	pyplot.plot( x, X_poly.dot(theta), linestyle='--', linewidth=3 )
 
 def learningCurve( X, y, X_val, y_val, lamda ):
 	m = shape( X )[0]
@@ -88,6 +110,33 @@ def learningCurve( X, y, X_val, y_val, lamda ):
 	return error_train, error_val
 
 
+def validationCurve( X, y, X_val, y_val ):
+	lamda_vec = array([0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0]).T
+	error_train = []
+	error_val = []
+
+	for lamda in lamda_vec:
+		cost, theta = train( X, y, lamda )
+
+		error_train.append( computeCost( theta, X, y, lamda ) )
+		error_val.append( computeCost( theta, X_val, y_val, lamda ) )
+
+	error_train = array( error_train )
+	error_val = array( error_val )
+
+	pyplot.ylabel('Error')
+	pyplot.xlabel('Lambda')
+	pyplot.plot( lamda_vec, error_train, 'b', label='Train' )
+	pyplot.plot( lamda_vec, error_val, 'g', label='Cross Validation' )
+	pyplot.legend()
+	pyplot.show( block=True )
+
+	return error_train, error_val
+
+
+
+
+
 print linearRegCostfunction( theta, c_[ones((shape(X)[0], 1)), X  ] , y, 1.0 )
 
 print "Training "
@@ -107,3 +156,45 @@ pyplot.show()
 
 lamda = 0.0
 print learningCurve( X, y, X_val, y_val, lamda )
+
+print "Feature mapping for polynomial regression"
+
+p = 8
+m, n = shape( X )
+
+X_poly = polyFeatures( X, p )
+X_poly, mu, sigma = featureNormalize( X_poly )
+X_poly = c_[ones((m, 1)), X_poly]
+
+X_poly_test = polyFeatures( X_test, p )
+X_poly_test = X_poly_test - mu
+X_poly_test = X_poly_test / sigma
+X_poly_test = c_[ones(( shape(X_poly_test)[0], 1)), X_poly_test]
+
+X_poly_val = polyFeatures( X_val, p )
+X_poly_val = X_poly_val - mu
+X_poly_val = X_poly_val / sigma
+X_poly_val = c_[ones(( shape(X_poly_val)[0], 1)), X_poly_val]
+
+print X_poly[0, :]
+
+lamda = 0.0
+
+cost, theta = train( X_poly, y, lamda )
+
+pyplot.scatter( X, y, marker='x', c='r', s=30, linewidth=2 )
+pyplot.xlim([-80, 80])
+pyplot.ylim([-60, 40])
+pyplot.xlabel('Change in water level(x)')
+pyplot.ylabel('Water flowing out of the dam(y)')
+
+pyplot.text( -15, 45, 'Lambda = %.1f' %lamda )
+plotFit( min(X), max(X), mu, sigma, theta, p )
+
+pyplot.show()
+
+learningCurve( X_poly, y, X_poly_val, y_val, lamda )
+
+print "Validation of the data"
+
+validationCurve( X_poly, y, X_poly_val, y_val )
